616da43c73937890258c3ef4e20c4cf439f08cab
Alessandro <38206235+avalenti8@users.noreply.github.com>
2019-10-28 16:51:38 +0100

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+int array[5];
+
+for(int i=0; i<5; i++) {
+
+cout << "inserisci un numero: ";
+cin >> array[i];
+
+}
+
+for(int a=0; a<5; a++) {
+
+cout << array[a] << ", ";
+
+}
+	return 0;
+}
+
